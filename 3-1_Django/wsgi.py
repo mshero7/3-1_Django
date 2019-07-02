a10d90938a1a74cf83a4e80b9d8e91a2efa62e80
mshero7 <mshero7@naver.com>
2019-07-02 19:01:35 +0900

    complete

--- 3-1_Django/wsgi.py
+++ 3-1_Django/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PythonWebProgramming.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '3-1_Django.settings')
 
 application = get_wsgi_application()
 
